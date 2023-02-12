ab9aef0e92bb6c304934e191b5f025289d790183
kudryashovalexey <gvinpin72@gmail.com>
2023-02-12 15:58:56 +0300

    transform matrix, pan, pinch and rotation handlers

--- components/Stickers/Stickers.tsx
+++ components/Stickers/Stickers.tsx
@@ -12,6 +12,12 @@ import { HelloSticker, HelloStickerDimensions } from "./HelloSticker";
 import { LocationSticker, LocationStickerDimensions } from "./LocationSticker";
 import { GestureHandler } from "./GestureHandler";
 import { Picture, PictureDimensions } from "./Picture";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+} from "react-native-reanimated";
+import { identity4, Matrix4, multiply4 } from "react-native-redash";
 
 const { width, height } = Dimensions.get("window");
 
@@ -21,14 +27,37 @@ const aveny = require("./assets/aveny.ttf");
 export const Stickers = () => {
   const image = useImage(zurich);
   const font = useFont(aveny, 56);
+  const matrix = useSharedValue(identity4);
+  const pan = Gesture.Pan().onChange((e) => {
+    matrix.value = multiply4(
+      matrix.value,
+      Matrix4.translate(e.changeX, e.changeY, 0)
+    );
+  });
+  const pinch = Gesture.Pinch().onChange((e) => {
+    matrix.value = multiply4(
+      matrix.value,
+      Matrix4.scale(e.scaleChange, e.scaleChange, 1)
+    );
+  });
+  const rotation = Gesture.Rotation().onChange((e) => {
+    matrix.value = multiply4(matrix.value, Matrix4.rotateZ(e.rotationChange));
+  });
+  const style = useAnimatedStyle(() => ({
+    transform: [{ matrix: matrix.value as unknown as number[] }],
+  }));
   if (!image || !font) {
     return null;
   }
   return (
     <View>
-      <Canvas style={{ width, height }}>
-        <Picture image={image} />
-      </Canvas>
+      <GestureDetector gesture={Gesture.Race(pan, pinch, rotation)}>
+        <Animated.View style={style}>
+          <Canvas style={{ width, height }}>
+            <Picture image={image} />
+          </Canvas>
+        </Animated.View>
+      </GestureDetector>
     </View>
   );
 };
