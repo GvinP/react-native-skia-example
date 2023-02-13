86edf5ffec659aae6072c8ec4eb88ae26c71393a
kudryashovalexey <gvinpin72@gmail.com>
2023-02-13 07:28:54 +0300

    glassmorphism

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { Stickers } from "./components/Stickers";
+import Glassmorphism from "./components/Glassmorphism";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.container}>
-        <Stickers />
+        <Glassmorphism />
         <StatusBar style="auto" />
       </View>
     </GestureHandlerRootView>
