ef32a2ac2a3a7cf7b975310f441f3c20e0b56861
kudryashovalexey <gvinpin72@gmail.com>
2023-02-14 16:37:08 +0300

    matrix animation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import CircleChart from "./components/CircleChart";
+import { Matrix } from "./components/Matrix";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.container}>
-        <CircleChart />
+        <Matrix />
         <StatusBar style="auto" />
       </View>
     </GestureHandlerRootView>
