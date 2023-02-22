688b0936dbd03e5d2e39e121568b1de87f0c77e8
kudryashovalexey <gvinpin72@gmail.com>
2023-02-22 19:59:58 +0300

    add cursor to graph

--- components/D3Chart/index.tsx
+++ components/D3Chart/index.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View, Pressable } from "react-native";
 import React from "react";
-import * as d3 from "d3";
 import {
   Canvas,
+  Circle,
   Easing,
+  Group,
   Line,
+  LinearGradient,
   Path,
   runTiming,
   Skia,
@@ -12,9 +14,11 @@ import {
   useComputedValue,
   useValue,
   vec,
+  useTouchHandler,
 } from "@shopify/react-native-skia";
 import { animatedData, DataPoint, originalData } from "./Data";
 import { curveBasis, line, scaleLinear } from "d3";
+import { getYForX, parse } from "react-native-redash";
 
 interface GraphData {
   min: number;
@@ -31,6 +35,15 @@ const D3Chart = () => {
     currentChart: 0,
     nextChart: 1,
   });
+  const x = useValue(0);
+  const onTouch = useTouchHandler({
+    onStart: () => {},
+    onActive: (pt) => {
+      x.current = pt.x;
+    },
+    onEnd: () => {},
+  });
+
   const makeGraph = (data: DataPoint[]): GraphData => {
     const min = Math.min(...data.map((val) => val.value));
     const max = Math.max(...data.map((val) => val.value));
@@ -63,34 +76,52 @@ const D3Chart = () => {
     const result = start.interpolate(end, isTransitionComplete.current);
     return result?.toSVGString() ?? "";
   }, [transitionState, isTransitionComplete]);
+
+  const y = useComputedValue(() => {
+    return getYForX(parse(currentPath.current), x.current) || 0;
+  }, [x, currentPath]);
+  const transform = useComputedValue(
+    () => [{ translateX: x.current }, { translateY: y.current }],
+    [x, y]
+  );
+
   return (
     <View style={styles.container}>
       <Canvas
         style={{
           height: GRAPH_HEIGHT,
           width: GRAPH_WIDTH,
-          //   borderWidth: 1,
         }}
+        onTouch={onTouch}
       >
-        <Line
-          p1={vec(10, 130)}
-          p2={vec(400, 130)}
-          color="lightgrey"
-          strokeWidth={1}
-        />
-        <Line
-          p1={vec(10, 250)}
-          p2={vec(400, 250)}
-          color="lightgrey"
-          strokeWidth={1}
-        />
-        <Line
-          p1={vec(10, 370)}
-          p2={vec(400, 370)}
-          color="lightgrey"
-          strokeWidth={1}
-        />
-        <Path path={currentPath} color="teal" strokeWidth={4} style="stroke" />
+        <Group>
+          <Line
+            p1={vec(10, 130)}
+            p2={vec(400, 130)}
+            color="lightgrey"
+            strokeWidth={1}
+          />
+          <Line
+            p1={vec(10, 250)}
+            p2={vec(400, 250)}
+            color="lightgrey"
+            strokeWidth={1}
+          />
+          <Line
+            p1={vec(10, 370)}
+            p2={vec(400, 370)}
+            color="lightgrey"
+            strokeWidth={1}
+          />
+          <Path path={currentPath} strokeWidth={4} style="stroke">
+            <LinearGradient
+              start={vec(0, 0)}
+              end={vec(GRAPH_WIDTH, 0)}
+              colors={["#ff00ff", "#00ff00"]}
+            />
+          </Path>
+          <Circle cx={0} cy={0} r={9} color="#ffa200" transform={transform} />
+        </Group>
       </Canvas>
       <View style={styles.buttonContainer}>
         <Pressable
